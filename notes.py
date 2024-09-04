a7bcf937106b767619db72e383775f837a1b5699
Darshbarot0 <darshbarot18@gmail.com>
2024-09-04 14:53:31 +0000

    I finished taking notes going to start program 2.

---
+++ notes.py
@@ -0,0 +1,18 @@
+name = input("Please tell me your name!")
+print("Hi + name)
+
+name = input("Give me another name \n\t")
+
+print("Hi " + name)
+print(name + " is a pretty cool name!")
+print(" I wish my name was " +  name)
+
+
+
+
+name = input("Tell me yoiur name now! ")
+email = input("Let me send you span ")
+nickname = input("what do your freinds call you?  )
+
+print(Hey  " + name + ", or should I say " + nickname +".")
+print("I am going to sende you a bunch of spat at " + email)
